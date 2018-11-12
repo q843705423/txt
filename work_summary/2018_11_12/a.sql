d511f4f380257e821219cb827e1fbb9bffaa3067
其乐无穷 <843705423@qq.com>
2018-11-12 21:16:57 +0800

    add new work summary;

---
+++ work_summary/2018_11_12/a.sql
@@ -0,0 +1,7 @@
+prompt Importing table PI_BUSACTIONLOG...
+set feedback off
+set define off
+insert into PI_BUSACTIONLOG (RECID, ACTIONTIME, ACTIONTYPE, REMARK, BUSID, DEPTID, PLACE)
+values (24478668, to_date('12-11-2018 20:12:40', 'dd-mm-yyyy hh24:mi:ss'), 1, null, '9-7519', '090101', null);
+
+prompt Done.
