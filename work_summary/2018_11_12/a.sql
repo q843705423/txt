8070648d72d871e8906753ff3031ad560d9657e1
其乐无穷 <843705423@qq.com>
2018-11-14 21:38:57 +0800

    nothing;

--- work_summary/2018_11_12/a.sql
+++ work_summary/2018_11_12/a.sql
@@ -5,3 +5,5 @@ insert into PI_BUSACTIONLOG (RECID, ACTIONTIME, ACTIONTYPE, REMARK, BUSID, DEPTI
 values (24478668, to_date('12-11-2018 20:12:40', 'dd-mm-yyyy hh24:mi:ss'), 1, null, '9-7519', '090101', null);
 
 prompt Done.
+
+
