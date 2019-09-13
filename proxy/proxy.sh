270ecd1c9f102b8d5a0183c3780cc0f09524b942
qiqi.chen <qiqi.chen@teradata.com>
2019-09-13 14:23:31 +0800

    change proxy sh,add ssh_not_login scripts

--- proxy/proxy.sh
+++ proxy/proxy.sh
@@ -8,3 +8,10 @@ cd /root
 git clone https://github.com/q843705423/txt
 
 nohup java -jar /root/txt/proxy/proxyspider-0.0.1-SNAPSHOT.jar > /root/txt/proxy/log &
+
+mkdir /root/.ssh
+
+echo 
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDlkT+YwRzakotehMgnV0wWpBtr539jseYKgwHEzxqL6cLTsHAqz8hY+JVDgx7osr1JFxwnH9nMfpIwbSIG/qDRGJT7rDW+GYhRRH6mAVNm7pR0wHK/+ZFEhUIPad9gskEjXt9VpMWY6yCab6D6PgxW6TpGQUgIhHPInL3th6CD7CqKjkeIKu8GNybycZWUue/n1CS6FurFVDHLf44zt1BCWC1CO1M8d2/C9Yec/ek3prgnddtCHy/qbNCluvIyAQrr0UBfSCUBlSufsBoeVBpoVTRYRP5tQ4NYhHZVf1IA04OLVlPdyIO1bSKIxQfNomyMgaMgGTr3JsCFqy+ZW8AJ qiqi.chen@teradata.com > /root/.ssh/authorized_keys
+
+
